530740a7499d22f96e9d147f5511e885238edaef
Babar <babarshahzad0@gmail.com>
2024-02-07 00:38:12 +0000

    Added new code for Register and Login

---
+++ StudentPortal/StudentPortal/BL/Utility.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentPortal.BL
+{
+    public static class Utility
+    {
+        private static string IDPrefix = "C";
+
+        public static string GenerateId(int counter)
+        {
+            string id = $"C{counter:D4}";
+            return id;
+        }
+    }
+}
